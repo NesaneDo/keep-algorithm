3a57460b1f31c2dbc5e60ef736a70aa288c1c097
Giant0 <1962741694@qq.com>
2020-09-24 08:35:54 +0800

    LeetCode (easy)#501 Find Mode in Binary Search Tree

--- src/util/TreeNode.java
+++ src/util/TreeNode.java
@@ -26,19 +26,19 @@ public class TreeNode {
 		 * 
 		 * ********** / ** \
 		 * 
-		 * ******** 3 ****** 6
+		 * ******** 1 ****** 6
 		 * 
 		 * ****** / ** \ ***** \
 		 * 
-		 * *****1 ****** 2 ***** 8
+		 * *****1 ****** 3 ***** 6
 		 */
 		TreeNode root = new TreeNode(5);
 		TreeNode[] nodes = new TreeNode[5];
-		nodes[0] = new TreeNode(3);
+		nodes[0] = new TreeNode(1);
 		nodes[1] = new TreeNode(6);
 		nodes[2] = new TreeNode(1);
-		nodes[3] = new TreeNode(2);
-		nodes[4] = new TreeNode(8);
+		nodes[3] = new TreeNode(3);
+		nodes[4] = new TreeNode(6);
 		root.left = nodes[0];
 		root.right = nodes[1];
 		nodes[0].left = nodes[2];
