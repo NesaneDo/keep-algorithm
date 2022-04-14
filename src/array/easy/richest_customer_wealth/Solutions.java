75a8b17e37e711abf936c7d1139a15ae62d1ef2b
Rondo <1962741694@qq.com>
2022-04-14 10:11:54 +0800

    LeetCode 1672 Richest Customer Wealth

--- src/array/easy/richest_customer_wealth/Solutions.java
+++ src/array/easy/richest_customer_wealth/Solutions.java
@@ -1,4 +1,17 @@
 package array.easy.richest_customer_wealth;
 
+import java.util.Arrays;
+
+/**
+ * 1672. 最富有客户的资产总量
+ * @author Rondo Chan
+ * @desc 1672. 最富有客户的资产总量
+ * @since 2022/4/14
+ * @version 1.0.0
+ */
 public class Solutions {
+    public int maximumWealth(int[][] accounts) {
+        // 慢
+        return Arrays.stream(accounts).mapToInt(a->Arrays.stream(a).sum()).max().getAsInt();
+    }
 }
